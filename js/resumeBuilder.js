bc4eda0e8b5de8d5b58e3532c86a853bfbf559cc
Steven C. Toy <you@example.comtoy@beema.com>
2016-12-27 14:27:02 -0500

    Changes after first day of Java Class.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,121 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+
+
+var bio = {
+  "name" : "Steven C. Toy",
+  "role" : "CEO",
+  "contact" : {
+    "mobile" : "918-486-9269",
+    "email" : "steve@stevenctoy.com",
+    "twitter" : "@sctoy",
+    "github" : "https://github.com/sctoy",
+    "web" : "http://stevenctoy.com",
+    "location" : "New York, NY"
+  },
+  "welcome" : "Seasoned executive with 25+ years of experience, \
+    building start-ups—early, mid and late stage as well as internal \
+    ventures—leading revenue up to $100M across verticals. Experience \
+    across Finance, Operations, HR, Sales and Marketing. Milestones: \
+    Led TaxChat to acquisition in 8 months. Secured $30M+ funding across \
+    companies and rounds.",
+  "skills" : ["P&L", "Start-Ups", "Fundraising", "Financial Analysis",
+  "Board & Governance", "Product Development", "Business Development",
+  "Metrics-Driven Insight", "Strategic Partnerships", "Cross-Border Teams",
+  "Public Speaking", "Negotiations"],
+  "bioPic" : "images/SCT_Headshot_200x250.jpg"
+};
+
+var education = {
+  "schools" : [
+    {
+      "college" : "Swarthmore",
+      "city" : "Swarthmore, PA",
+      "degree" : "Bachelor of Arts",
+      "majors" : ["Economics"],
+      "dates" : "1985-1989",
+      "url" : "http://www.swarthmore.edu/"
+    }
+  ],
+  "onineCourses" : [
+    {
+      "title" : "JavaScript Crash Course",
+      "school" : "Udacity",
+      "dates" : "2016",
+      "url" : "http://www.udacity.com/course/ud804"
+    }
+  ]
+}
+
+var work = {
+  "jobs" : [
+    {
+      "employer" : "Ernst & Young",
+      "title" : "Senior Manager",
+      "dates" : "November 2016 - Present",
+      "description" : "This is the description for EY. I don't have a lot \
+        to say at this time. I need to figure out how to deal with the \
+        carriage return in Atom."
+    },
+    {
+      "employer" : "TaxChat {Acquired by EY}",
+      "title" : "CEO",
+      "dates" : "January 2016 - November 2016",
+      "description" : "This is just greeked text for the moment as I don't\
+        have a ton to say and my actual resume uses bullets more than just \
+        text so I will need to figure out that formatting eventually."
+    },
+    {
+      "employer" : "Fareportal Inc.",
+      "title" : "SVP - Hotels, Cars, Ancillary",
+      "dates" : "April 2013 - December 2015",
+      "description" : "This is just greeked text for the moment as I don't\
+        have a ton to say and my actual resume uses bullets more than just \
+        text so I will need to figure out that formatting eventually."
+    }
+  ]
+}
+
+var projects = {
+  "jobs" : [
+    {
+      "title" : "Sample Project 1",
+      "dates" : "2016",
+      "description" : "This is the description for EY. I don't have a lot \
+        to say at this time. I need to figure out how to deal with the \
+        carriage return in Atom.",
+      "images" : [
+        "images/TaxChat1.png",
+        "images/TaxChat2.png"
+      ]
+    }
+  ]
+}
+
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+var formattedContact = HTMLcontactGeneric.replace("%contact%", bio.contact[0] &
+    "%data%", bio.contact[1] );
+var formattedPic = HTMLbioPic.replace("%data%", bio.bioPic);
+var formattedWelcome = HTMLwelcomeMsg.replace("%data%", bio.welcome);
+
+$("#header").prepend(formattedWelcome);
+$("#header").prepend(formattedPic);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+//$("#header").append(formattedContact);
+
+
+
+
+// Code to prevent malicious html from being added
+var html = '<script src="http://hackyourwebsite.com/eviljavascript.js"></script>';
+
+var charEscape = function(_html) {
+    var newHTML = _html;
+
+    newHTML = _html.replace(/</g, "&lt;");
+    newHTML = newHTML.replace(/>/g, "&gt;");
+
+    return newHTML;
+};
+
+console.log(charEscape(html));
---
+++ scrap.js
@@ -0,0 +1,99 @@
+/*
+This is empty on purpose! Your code to build the resume will go here.
+ */
+//$("#main").append("Steven C. Toy");
+
+//var firstName = "Steven";
+
+//var age = 49;
+
+//console.log(firstName);
+
+//var awesomeThoughts = "I am Steve and I am Awesome!";
+
+//console.log(awesomeThoughts);
+
+//var funThoughts = awesomeThoughts.replace("Awesome", "Fun");
+
+//$("#main").append(funThoughts);
+//console.log(awesomeThoughts);
+//console.log(funThoughts);
+
+//var name = "Steven C. Toy";
+//var role = "CEO";
+
+//$("#main").append(bio.firstName);
+
+
+//console.log(formattedName);
+//console.log(formattedRole);
+
+//var name = "AlbERt EINstEiN";
+
+//function nameChanger(oldName) {
+    //var finalName = oldName;
+    // Your code goes here!
+    //var names = oldName.split(" ");
+    //names[1] = names[1].toUpperCase();
+    //names[0] = names[0].slice(0,1).toUpperCase() + names[0].slice(1).toLowerCase();
+    //finalName = names.join(" ");
+
+    // Don't delete this line!
+    //return finalName;
+//};
+
+// Did your code work? The line below will tell you!
+//console.log(nameChanger(name));
+
+// Practice using dot notation
+var workEY = {}
+workEY.company = "EY";
+workEY.city = "New York, NY";
+workEY.start = "November 2016";
+workEY.end = "Present";
+workEY.title = "Senior Manager";
+
+// Formatting header objects
+var formattedName = HTMLheaderName.replace("%data%", bio.fullName);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+var formattedContact = HTMLcontactGeneric.replace("%contact%", bio.contact[0] &
+    "%data%", bio.contact[1] );
+var formattedPic = HTMLbioPic.replace("%data%", bio.headshot);
+var formattedWelcome = HTMLwelcomeMsg.replace("%data%", bio.welcome);
+
+// Formatting contact info
+var formattedMobile = HTMLmobile.replace("%data%", bio.mobile);
+var formattedEmail = HTMLemail.replace("%data%", bio.email);
+var formattedTwitter = HTMLtwitter.replace("%data%", bio.twitter);
+var formattedGitHub = HTMLgithub.replace("%data%", bio.github);
+var formattedWeb = HTMLblog.replace("%data%", bio.web);
+var formattedLocation = HTMLlocation.replace("%data%", bio.location);
+
+// Formatting first job
+var formattedWorkComp = HTMLworkEmployer.replace("%data%", workEY.company);
+var formattedWorkTitle = HTMLworkTitle.replace("%data%", workEY.title);
+var formattedWorkDate = HTMLworkDates.replace("%data%", workEY.start + " - "
++ workEY.end);
+
+var formattedSchool = HTMLschoolName.replace("%data%", eduSwat.college);
+
+$("#header").prepend(formattedWelcome);
+$("#header").prepend(formattedPic);
+$("#header").prepend(formattedMobile);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+$("#header").append(formattedWorkTitle);
+$("#header").append(formattedSchool);
+//$("#header").append(formattedEmail);
+//$("#header").append(formattedPic);
+//$("#header").append(formattedWelcome);
+
+$("#workExperience").prepend(formattedWorkComp);
+$("#workExperience").append(formattedWorkTitle);
+$("#workExperience").append(formattedWorkDate);
+
+console.log(formattedWorkComp);
+
+//$("#lets-connect").append(formattedTwitter);
+//$("#lets-connect").append(formattedGitHub);
+//$("#lets-connect").append(formattedWeb);
